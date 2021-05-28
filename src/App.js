6696c139b6fcd5a823a02c58603e6ed1ce1775d6
Nicola Winterson <nicola.l.winterson@gmail.com>
2021-05-28 09:33:34 +0200

    react router dom implementation

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 import './App.css';
 import Home from "./components/Home"
+import About from "./components/About"
 
 function App() {
   return (
-    <div className="App">
-      <Home />
-    </div>
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route path="/about" component={About} />
+      </Switch>
+    </BrowserRouter>
   );
 }
 
---
+++ src/components/About.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const About = () => {
+    return (
+        <div>
+            <div>
+                <h1>Hej and welcome to ABOUT</h1>
+            </div>
+            <footer></footer>
+        </div>
+    )
+}
+
+export default About
