02982f3fb1c88d9c0c4698e6b8546577630bf06e
Nicola Winterson <nicola.l.winterson@gmail.com>
2021-05-28 11:41:13 +0200

    implemented data.json

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,10 +1,37 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 
 const SearchBar = (props) => {
+    const [data, setData] = useState([]);
+    const getData=()=>{
+        fetch('data.json'
+        ,{
+          headers : { 
+            'Content-Type': 'application/json',
+            'Accept': 'application/json'
+           }
+        }
+        )
+          .then(function(response){
+            console.log(response)
+            return response.json();
+          })
+          .then(function(myJson) {
+            console.log(myJson);
+            setData(myJson)
+          });
+      }
+      useEffect(()=>{
+        getData()
+      },[])
+
+
     return (
         <div>
-                <input placeholder={props.placeholder}></input>
-                <button>Search</button>
+            <input placeholder={props.placeholder}></input>
+            <button>Search</button>
+            <div>
+                {data && data.length>0 && data.map((item)=><p>{item.about}</p>)}
+            </div>
         </div>
     )
 }
