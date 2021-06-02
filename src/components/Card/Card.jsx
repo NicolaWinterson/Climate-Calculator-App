c585932a239cacdccef589a4aa6722b250385988
Nicola Winterson <nicola.l.winterson@gmail.com>
2021-06-02 12:15:20 +0200

    implented card component

---
+++ src/components/Card/Card.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Card = () => {
+    return (
+        <div>
+            <p> I'm a card component!</p>
+        </div>
+    )
+}
+
+export default Card
--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import SearchBar from "../SearchBar/SearchBar.jsx"
 import Navbar from "../Navigation/Navbar"
+import Card from "../Card/Card"
 import "./styles.css"
 
 const Home = () => {
@@ -9,7 +10,11 @@ const Home = () => {
             <Navbar />
             <div className="home__background">
                 <div className="home__top"><h1 className="home__heading">Hej and welcome to HOME</h1></div>
-                <div className="home__content"><SearchBar placeholder={"Search..."} /></div>
+                <div className="home__content">
+                    <SearchBar placeholder={"Search..."} />
+                    <Card />
+                </div>
+                
             </div>
             
         </div>
