cdc7a849a24823fb2136313f8bde1c524340d924
Nicola Winterson <nicola.l.winterson@gmail.com>
2021-06-04 11:31:09 +0200

    update to db access function

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,22 +1,52 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import SearchBar from "../SearchBar/SearchBar.jsx"
 import Navbar from "../Navigation/Navbar"
 import Card from "../Card/Card"
 import "./styles.css"
 
 const Home = () => {
+    const [value, setValue] = useState("")
+    const [data, setData] = useState([]);
+    const getData = () => {
+        fetch('../db/data.json'
+            , {
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Accept': 'application/json'
+                }
+            }
+        )
+            .then(function (response) {
+                console.log(response)
+                return response.json();
+            })
+            .then(function (myJson) {
+                console.log(myJson);
+                setData(myJson)
+            }, (error) => {
+                if (error) {
+                    console.log("data not found")
+                }
+            }
+
+            );
+    }
+    useEffect(() => {
+        getData()
+    }, [])
+
     return (
         <div>
             <Navbar />
             <div className="home__background">
                 <div className="home__top"><h1 className="home__heading">Hej and welcome to HOME</h1></div>
                 <div className="home__content">
-                    <SearchBar placeholder={"Search..."} />
+                    <SearchBar placeholder={"Search..."} onChange={(value) => setValue(value)} onSubmit={getData} />
                     <Card />
                 </div>
-                
+
             </div>
-            
+
         </div>
     )
 }
--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -1,36 +1,14 @@
-import React, {useEffect, useState} from "react";
+import React from "react";
 import Checkboxes from "../Checkboxes/Checkboxes"
 import "./styles.css"
 
 const SearchBar = (props) => {
-    /* const [data, setData] = useState([]);
-    const getData=()=>{
-        fetch('data.json'
-        ,{
-          headers : { 
-            'Content-Type': 'application/json',
-            'Accept': 'application/json'
-           }
-        }
-        )
-          .then(function(response){
-            console.log(response)
-            return response.json();
-          })
-          .then(function(myJson) {
-            console.log(myJson);
-            setData(myJson)
-          });
-      }
-      useEffect(()=>{
-        getData()
-      },[])
- */
 
     return (
         <>
-            <input className="search__input" placeholder={props.placeholder}></input>
+            <input className="search__input" placeholder={props.placeholder} onSubmit={props.onSubmit}></input>
             <Checkboxes />
+            <button onSubmit={props.onSubmit}></button>
         </>
     )
 }
